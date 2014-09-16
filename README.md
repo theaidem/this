6fc7abc44591877362aa6205af0210278ce95692
Max Kokorin <kokorin.max@gmail.com>
2014-09-17 02:47:30 +0700

    Edit readme/add flag

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ this
 ====
 
 Simple File Server for current folder
+
+`go get githib.com/theaidem/this`
+
+`cd $GOPATH/githib.com/theaidem/this`
+
+`go build`
+
+`sudo cp ./this /usr/bin`
--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
-var port = ":1313"
+var port = flag.String("port", "1313", "Your custom port")
 
 func main() {
+	flag.Parse()
 	if dir, err := os.Getwd(); err == nil {
 		log.Println("Current path: ", dir)
 		http.Handle("/", http.FileServer(http.Dir(dir)))
-		log.Println("Server listen localhost", port)
-		log.Fatal(http.ListenAndServe(port, nil))
+		log.Println(fmt.Sprintf("Server listen http://localhost:%s", (*port)))
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", (*port)), nil))
 	} else {
 		log.Fatal(err)
 	}
